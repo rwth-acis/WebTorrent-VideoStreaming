8cf93e2d291183550d5039bf553269337e1e65f0
Philipp Bartels <Philipp.Bartels@rwth-aachen.de>
2016-07-26 23:07:40 +0200

    Some more corrections for JSDoc Docu

--- OakStreaming/index.js
+++ OakStreaming/index.js
@@ -27,7 +27,7 @@ function OakStreaming(OakName){
       //console.log("Version: Archer   In OakStreaming constructor. this.name: " + OakName);
       
       // Only methods should be part of the API, i.e. only methods should be publically accessible.
-      // Every method should have access to these variables. Therefore they are definied at this high scope.
+      // Every method should have access to these variables. Therefore they are defined at this high scope.
       var simplePeerCreationCounter = 0;
       var connectionsWaitingForSignalingData = [];
       var theTorrent = null;
@@ -42,7 +42,7 @@ function OakStreaming(OakName){
       self.forTesting_connectedToNewWebTorrentPeer = null;
 
       
-      /** This method returns the number of bytes downloaded from the Web server server.
+      /** This method returns the number of bytes downloaded from the Web server.
       * @pulic
       * @method
       * @returns {Number}
@@ -203,25 +203,25 @@ function OakStreaming(OakName){
        /**
        * @typedef Stream_Information
        * @type {object}
-       * @property {number} video_file_size - The size in byte of the video file that was passed as an argument.
+       * @property {number} video_file_size - The size in byte of the video file that was passed as the first argument to this method.
        */
        
       /**
        * @callback OakStreaming~streamVideoFinished
-       * @param {Stream_Information} stream_information_object - An object that other OakStreaming instances can pass as an argument to their loadVideo method to download the video from this and other OakStreaming instances and/or a Web Server.
+       * @param {Stream_Information} stream_information_object - An object that other OakStreaming instances can pass as an argument to their loadVideo method to download the video from this and other OakStreaming instances and/or a Web server.
        */
 
       /**
-       * This method creates a Stream_Information object that other OakStreaming instances can pass as an argument to their loadVideo method to download the video from this and other OakStreaming instances and/or a Web Server.
-       * @param {object} [video_file] - The video file that should be streamed peer-to-peer to other OakStreaming instances. This paramter can either be a {@link https://developer.mozilla.org/en-US/docs/Web/API/File |W3C File object}, a {@link https://developer.mozilla.org/en-US/docs/Web/API/FileList |W3C FileList}, a {@link https://nodejs.org/api/buffer.html |Node Buffer object} or a {@link https://nodejs.org/api/stream.html#stream_class_stream_readable |Readable stream object}.
-       * @param {object} [options] - Options for the creation of the Stream_Information object. After its creation, the Stream_Information object gets passed by the streamVideo method as an argument to the callback function.
-       * @param {string} [options.web_server_URL] - URL of a Web server that can serve the video file. If this property is not set, XHRs will be send to the Web server that served the Web page. If this property is set to false, no Web server will be requested.
+       * This method creates a Stream_Information object that other OakStreaming instances can pass as an argument to their loadVideo method to download the video from this and other OakStreaming instances and/or a Web server.
+       * @param {object} video_file - The video file that should be streamed peer-to-peer to other OakStreaming instances. This paramter can either be a {@link https://developer.mozilla.org/en-US/docs/Web/API/File |W3C File object}, a {@link https://developer.mozilla.org/en-US/docs/Web/API/FileList |W3C FileList}, a {@link https://nodejs.org/api/buffer.html |Node Buffer object} or a {@link https://nodejs.org/api/stream.html#stream_class_stream_readable |Readable stream object}.
+       * @param {object} options - Options for the creation of the Stream_Information object. After its creation, the Stream_Information object gets passed by the streamVideo method as an argument to the callback function.
+       * @param {string} [options.web_server_URL] - URL of a Web server that can serve the video file. If this property is not set, XML HTTP Requests (XHRs) will be send to the Web server that served the Web page. If this property is set to false (not only falsy), no Web server will be requested.
        * @param {number} [options.web_server_port = 80] - Port that will be used when communicating with the Web server that was specified in the web_server_URL property. This property should only be set when the web_server_URL property is set too.
-       * @param {string} [options.path_to_file_on_Web_server] - This path will be used for the XML HTTP Requests (XHRs). For example, a valid path could be "/videos/aVideoFile.mp4". For successfull XHRs, it is not necessary to set both the path_to_file_on_Web_server and the hash_value paramter. If both properties are definied, the hash_value property will be used for XHRs. If this property and the hash_value property is undefined, no video data will be requested from the Web server.
-       * @param {string} [options.hash_value] - The SHA-256 hash value of the video file that should by (partially) requested from the Web server. It is not necessary to set both the path_to_file_on_Web_server and the hash_value paramter for successfull XML HTTP Requests (XHRs). If both properties are definied, the hash_value property will be used for XHRs. If this property and the path_to_file_on_Web_server property is undefined, no video data will be requested from the Web server.
+       * @param {string} [options.path_to_file_on_Web_server] - This path will be used for the XML HTTP Requests (XHRs). For example, a valid path could be "/videos/aVideoFile.mp4". The default value of this property is "/" concatenated with the name of the file that is seeded.
+       * @param {string} [options.hash_value] - The SHA-256 hash value of the video file that should by (partially) requested from the Web server. If this hash_value property is defined, it instead of the path_to_file_on_Web_server property will be used for XHRs.
        * @param {number} [options.download_from_server_time_range = 5] - How many seconds of video playback must be buffered in advance such that no data is requested from the Web server.
        * @param {string[][]} options.webTorrent_trackers - Array of arrays of WebTorrent tracking server URLs (strings). These WebTorrent trackers will be used to connect to other OakStreaming instances. In which order these tracking server a contacted is described in {@link http://www.bittorrent.org/beps/bep_0012.html}.
-       * @param {number} [options.Sequential_Requests_time_range = 20] - How many seconds of video playback must be buffered in advance such that no data streams are requested from the WebTorrent network.
+       * @param {number} [options.Sequential_Requests_time_range = 20] - How many seconds of video playback must be buffered in advance such that no sequential data streams are requested from the WebTorrent network and instead video fragments are requested according to the rarest-peace-first strategy.
        * @param {number} [options.create_readStream_request_size = 5000000] - The size of the sequential byte range requests to the WebTorrent network. Keeping the default value is sufficient for most use cases.
        * @param {number} [options.peer_upload_limit_multiplier = 2] - The OakStreaming client will severly throttle the video data upload to other peers when (bytes_uploaded_to_other_peers * peer_upload_limit_multiplier + peer_upload_limit_addend >  bytes_downloaded_from_other_peers). The OakStreaming client will stop the throtting as soon as the before mentioned inequality is no longer true.
        * @param {number} [options.peer_upload_limit_addend = 3000000] - The OakStreaming client will severly throttle the video data upload to other peers when (bytes_uploaded_to_other_peers * peer_upload_limit_multiplier + peer_upload_limit_addend >  bytes_downloaded_from_other_peers). The OakStreaming client will stop the throtting as soon as the before mentioned inequality is no longer true.
@@ -384,7 +384,7 @@ function OakStreaming(OakName){
        */ 
        
       /**
-       * Tries to receive the video stream described in the stream_information object. Offers received video data to all OakStreaming instances with whom it is connected.
+       * This method tries to receive the video stream described in the stream_information object. After this method has been called, the OakStreaming instance offers received video data to all OakStreaming instances with whom it is connected.
        * @param {Stream_Information} stream_information_object - This object contains all data that is needed to initiate downloading the video from other OakStreaming instances and/or a Web server. Stream_Information objects can be created by the {@link streamVideo|streamVideo} method.
        * @param {OakStreaming~downloadingVideoFinished} callback - This callback gets called when the video has been buffered entirely.
        * @param {boolean} end_streaming_when_video_downloaded - If this argument is true, all uploading to other OakStreaming instances is permanently cancelled and all processing of the loadVideo method permanently stopped as soon as the video has been downloaded completely.
@@ -454,7 +454,7 @@ function OakStreaming(OakName){
          
          
          // All these declared varibales until 'var self = this' are intended to be constants
-         var deliveryByServer = (stream_information_object.path_to_file_on_XHR_server || stream_information_object.hash_value) ? true : false;
+         var deliveryByServer = (stream_information_object.web_server_URL !== false && (stream_information_object.path_to_file_on_XHR_server || stream_information_object.hash_value)) ? true : false;
          var deliveryByWebtorrent = stream_information_object.torrentFile ? true : false;
          var XHRServerURL = stream_information_object.web_server_URL || false;
          var XHR_PORT = stream_information_object.web_server_port || 80;
